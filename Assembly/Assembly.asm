c631c3c7a170026516403b6ea40762ebe49ee712
韩凌昊 <417564526@qq.com>
2019-01-01 20:32:23 +0800

    aaa

--- Assembly/Assembly.asm
+++ Assembly/Assembly.asm
@@ -5,21 +5,10 @@ include kernel32.inc
 includelib msvcrt.lib
 includelib kernel32.lib
 
-$test_1 proto,
-    test_1_int_1$0:sdword
-$test_2 proto,
-    test_2_int_1$3:sdword,
-    temp: ptr sdword
-$test_3 proto,
-    test_3_int_array$6:ptr sdword,
-    test_3_int_1$7:sdword
-$main proto,
-    temp: ptr sdword
 .const
 $output_format_int byte "%d", 0DH, 0AH, 0
 $input_format_int byte "%d", 0
 .code
-
 $print proc,
     val: dword
     invoke crt_printf, addr $output_format_int, val
@@ -36,128 +25,88 @@ $$getch proc
 $$getch endp
 
 .data
-    global_int_1$0 sdword ?
-    global_int_2$0 sdword ?
-    global_int_array_1$1 sdword 10 dup(?)
+    a$0 sdword ?
+    b$1 sdword ?
+    temp$2 sdword ?
+    temp$3 sdword ?
+    temp$4 sdword ?
+    temp$5 sdword ?
+    temp$6 sdword ?
+    temp$7 sdword ?
 
 .code
-
-label100:
-$test_1 PROC,
-    test_1_int_1$0:sdword
-    LOCAL test_1_int_2$1:sdword,
-        temp$2:sdword,
-        $end_of_local:dword
-label101:
-label102:
-    INVOKE $test_2, test_1_int_1$0, ADDR temp$2
-label103:
-    mov eax, temp$2
-    mov test_1_int_2$1, eax
-label104:
-label105:
-    INVOKE $print, test_1_int_2$1
-label106:
-test_1_end:
-    ret
-$test_1 ENDP
-label200:
-$test_2 PROC,
-    test_2_int_1$3:sdword,
-    temp: ptr sdword
-
-    LOCAL test_2_int_2$4:sdword,
-        temp$5:sdword,
-        $end_of_local:dword
-label201:
-    mov eax, test_2_int_1$3
-    add eax, 10
-    mov temp$5, eax
-label202:
-    mov eax, temp$5
-    mov test_2_int_2$4, eax
-label203:
-    mov eax, test_2_int_2$4
-    mov esi, temp
-    mov [esi], eax
-    jmp test_2_end
-label204:
-test_2_end:
-    ret
-$test_2 ENDP
-label300:
-$test_3 PROC,
-    test_3_int_array$6:ptr sdword,
-    test_3_int_1$7:sdword
-    LOCAL test_3_int_2$8:sdword,
-        temp$9:sdword,
-        temp$10:sdword,
-        $end_of_local:dword
-label301:
-    mov eax, test_3_int_1$7
-    sub eax, 1
-    mov temp$9, eax
-label302:
-    mov eax, temp$9
-    imul eax, 4
-    mov temp$9, eax
-label303:
-    mov esi, temp$9
-    mov eax, test_3_int_array$6[esi]
-    mov temp$10, eax
-label304:
-    mov eax, temp$10
-    mov test_3_int_2$8, eax
-label305:
-    mov eax, test_3_int_2$8
-    mov esi, 0
-    mov test_3_int_array$6[esi], eax
-label306:
-    jmp test_3_end
-label307:
-test_3_end:
-    ret
-$test_3 ENDP
+$main:
 label0:
-$main PROC,
-    temp: ptr sdword
-
-    LOCAL main_int_1$11:sdword,
-        main_int_array_1$12[1]:sdword,
-        temp$13:sdword,
-        $end_of_local:dword
+    mov eax, 1
+    cmp eax, 0
+    jz label0
 label1:
-    mov eax, 10
-    mov esi, 0
-    mov main_int_array_1$12[esi], eax
+    mov eax, 1
+    mov temp$2, eax
 label2:
-    mov eax, 20
-    mov esi, 4
-    mov main_int_array_1$12[esi], eax
+    mov eax, temp$2
+    mov a$0, eax
 label3:
+    mov eax, temp$2
+    mov temp$3, eax
 label4:
-label5:
-    INVOKE $test_3, main_int_array_1$12, 2
+    mov eax, 1
+    cmp eax, 0
+    jz label4
+label0:
+    mov eax, 0
+    mov temp$4, eax
 label6:
-    mov esi, 0
-    mov eax, main_int_array_1$12[esi]
-    mov temp$13, eax
+    mov eax, temp$4
+    mov b$1, eax
 label7:
+    mov eax, temp$4
+    mov temp$5, eax
 label8:
-    INVOKE $print, temp$13
+    mov eax, 1
+    cmp eax, 0
+    jz label8
 label9:
-    mov eax, 10
-    mov main_int_1$11, eax
+    mov eax, 1
+    cmp eax, 0
+    jz label9
 label10:
+    mov eax, a$0
+    cmp eax, 0
+    jnz label14
 label11:
-    INVOKE $test_1, main_int_1$11
+    mov eax, b$1
+    cmp eax, 0
+    jnz label14
 label12:
     mov eax, 0
-    mov esi, temp
-    mov [esi], eax
-    jmp main_end
+    mov temp$6, eax
 label13:
-main_end:
-    ret
-$main ENDP
+    jmp label15
+label14:
+    mov eax, 1
+    mov temp$6, eax
+label15:
+    mov eax, temp$6
+    cmp eax, 0
+    jnz label18
+label16:
+    jmp label22
+label17:
+    jmp label22
+label18:
+    mov eax, 1
+    cmp eax, 0
+    jz label18
+label0:
+    mov eax, a$0
+    mov temp$7, eax
+label0:
+    mov eax, a$0
+    dec eax
+    mov a$0, eax
+label21:
+    jmp label8
+label22:
+    invoke $$getch
 END $main
