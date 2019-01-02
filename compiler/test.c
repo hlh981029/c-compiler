c416756c7f692972ffc312af60ec715eb96c3ca6
tDingQuan <tdingquan@gmail.com>
2019-01-02 12:02:28 +0800

    STRUCT

--- compiler/test.c
+++ compiler/test.c
@@ -1,4 +1,25 @@
 void main()
 {
+	int test1;
+	struct a{
+		int i;
+		int t;
+	} m;
 
+	struct b{
+		int i2;
+		int t2;
+	};
+	struct b m2;
+
+	struct {
+		int t3;
+		int i3;
+	} hbzz;
+
+	m.i = 10;
+	// test1 = m.i;
+	// m.t = m.i;
+	// m2.i2 = 100;
+	// hbzz.t3 = m2.i2;
 }
