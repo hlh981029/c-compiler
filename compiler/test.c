6e03beb4b66bb979ea7f8ade3d2f91ea36db36a8
韩凌昊 <417564526@qq.com>
2019-01-02 15:16:17 +0800

    123

--- compiler/test.c
+++ compiler/test.c
@@ -2,11 +2,16 @@ struct {
 	int a1,a2;
 	int a3;
 }b;
+
+
 void main() {
+	int a;
+	a = input();
 	b.a1 = 1;
 	b.a2 = b.a1;
-	b.a3 = 0;
+	b.a3 = a;
 	print(b.a1);
 	print(b.a2);
 	print(b.a3);
+	
 }
